ca076e9be5371ebf899c77a96a5b885f020c9714
Denys Stepanov <dex.stepanov@gmail.com>
2025-02-22 22:21:40 +0100

    docker2 - lesson 3

--- realworld-docker/api/src/index.js
+++ realworld-docker/api/src/index.js
@@ -1,14 +1,33 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const { connectDb } = require("./helpers/db");
 const { port, host, db} = require("./configuration");
 
 const app = express();
+
+const postSchema = new mongoose.Schema({
+    name: String
+});
+
+const Post = mongoose.model('Post', postSchema);
 
 const startServer = () => {
     app.listen(port, () => {
         console.log(`Started api service on port: ${port}`);
         console.log(`On host: ${host}`);
         console.log(`Our database: ${db}`);
+
+        // Post.find(function (err, posts) {
+        //     if (err) return console.log(err);
+        //     console.log('posts', posts);
+        // });
+
+        const post = new Post({ name: 'Silence' });
+
+        post.save(function (err, savedPosts) {
+            if (err) return console.log(err);
+            console.log('savedPosts with volumes!', savedPosts);
+        });
     });
 };
 
